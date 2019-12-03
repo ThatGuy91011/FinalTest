3d97f4b66fa6ace12f68e7fa8ff3aa9ddc5e60d7
KIRK WALLS <KIRWALLS@uat.edu>
2019-12-03 13:22:34 -0700

    Add project files.

---
+++ Person.cpp
@@ -0,0 +1,10 @@
+#include "Person.h"
+#include <iostream>
+
+using namespace std;
+
+
+void Person::Talk(string dialog)
+{
+	cout << dialog << endl;
+}
---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Person.h"
+using namespace std;
+
+int main()
+{
+	Person Sans;
+	Sans.Talk("ehehehehehehehehehe");
+
+	
+	return 0;
+}
